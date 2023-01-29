761288621265db56724926975766e2305de88f8b
Mohamed Badr <108628976+MohamedBadr552002@users.noreply.github.com>
2023-01-29 16:58:38 +0200

    Create C_basics2.c

---
+++ C Programing/C_basics2.c
@@ -0,0 +1,67 @@
+/*
+ * main.c
+ *
+ *  Created on: Jul 5, 2022
+ *      Author: Mohamed
+ */
+
+#include <stdio.h>
+
+void main()
+{
+	//Ex4
+
+   /*float a,b,c;
+	printf("enter two intger:");
+	fflush(stdout);
+	scanf("%f",&a);
+	scanf("%f",&b);
+	c=a*b;
+	printf("product:%f",c);*/
+
+	//Ex5
+	/*	char c;
+	printf("Enter a character:");
+	fflush(stdout);
+	scanf("%c",&c);
+	printf("ASCII value of %c: %d ",c,c);*/
+
+	//Ex6
+ /*  float a,b,temp;
+   printf("Enter value of a:");
+   fflush(stdout);
+   scanf("%f",&a);
+
+   printf("Enter value of b:");
+   fflush(stdout);
+   scanf("%f",&b);
+
+   temp=a;
+   a=b;
+   b=temp;
+   printf("\n");
+   printf("After swapping, value of a:%f\n",a);
+   printf("After swapping, value of a:%f",b);*/
+
+	//Ex7
+	/*   float a,b,temp;
+	   printf("Enter value of a:");
+	   fflush(stdout);
+	   scanf("%f",&a);
+
+	   printf("Enter value of b:");
+	   fflush(stdout);
+	   scanf("%f",&b);
+	   printf("\n");
+	   a=a+b;
+	   b=a-b;
+	   a=a-b;
+	   printf("After swapping, value of a:%f\n",a);
+	   printf("After swapping, value of a:%f",b);*/
+
+   //Ex1
+	printf("C programming");
+
+
+
+}

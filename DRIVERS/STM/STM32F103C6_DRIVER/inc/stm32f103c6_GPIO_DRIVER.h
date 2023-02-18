3f5b5693d2cd7692d49dd734c5c2e1551aabb984
Mohamed Badr <108628976+MohamedBadr552002@users.noreply.github.com>
2023-02-18 15:44:57 +0200

    Update stm32f103c6_GPIO_DRIVER.h

--- DRIVERS/STM/STM32F103C6_DRIVER/inc/stm32f103c6_GPIO_DRIVER.h
+++ DRIVERS/STM/STM32F103C6_DRIVER/inc/stm32f103c6_GPIO_DRIVER.h
@@ -1,230 +1,118 @@
 /*
- * stm32f103c6.h
+ * STM32F103C6_DRIVER_GPIO.h
  *
  *  Created on: Feb 16, 2023
  *      Author: Mohamed
  */
 
-#ifndef INC_STM32F103C6_H_
-#define INC_STM32F103C6_H_
+#ifndef INC_STM32F103C6_DRIVER_GPIO_H_
+#define INC_STM32F103C6_DRIVER_GPIO_H_
 
+//INCLUDES
+#include "stm32f103c6.h"
+
+//================================================================
 //-----------------------------
-//Includes
-//-----------------------------
-#include "stdlib.h"
-#include "stdint.h"
-
-//-----------------------------
-//Base addresses for Memories
-//-----------------------------
-#define FLASH_MEMORY_BASE    						0x08000000UL
-#define SYSTEM_MEMORY_BASE							0x1FFFF000UL
-#define SRAM_MEMORY_BASE 							0x20000000UL
-
-#define PERIPHERALS_BASE 							0x40000000UL
-#define CORTEX_M3_INTERNAL_PERIPHERALS_BASE    	    0xE0000000UL
-#define NVIC_BASE									0xE000E100UL
-
-#define NVIC_ISER0									*(volatile uint32_t *)(NVIC_BASE + 0x0)
-#define NVIC_ISER1									*(volatile uint32_t *)(NVIC_BASE + 0x4)
-#define NVIC_ISER2									*(volatile uint32_t *)(NVIC_BASE + 0x8)
-#define NVIC_ICER0									*(volatile uint32_t *)(NVIC_BASE + 0x80)
-#define NVIC_ICER1									*(volatile uint32_t *)(NVIC_BASE + 0x84)
-#define NVIC_ICER2									*(volatile uint32_t *)(NVIC_BASE + 0x88)
-
-
-//-----------------------------
-//Base addresses for AHP BUS Peripherals
-//-----------------------------
-        //RCC
-#define RCC_BASE  							    0x40021000UL
-
-//-----------------------------
-//Base addresses for APB2 BUS Peripherals
-//-----------------------------
-            //GPIO
-//A,B FULLY included in LQFP48 package
-#define GPIOA_BASE   						    0x40010800UL
-#define GPIOB_BASE   						    0x40010C00UL
-
-//C,D Partial included in LQFP48 package
-#define GPIOC_BASE                              0x40011000UL
-#define GPIOD_BASE                              0x40011400UL
-
-//E not included in LQFP48 package
-#define GPIOE_BASE                              0x40011800UL
-
-		  //EXTI
-#define EXTI_BASE  							    0x40010400UL
-
-		 //AFIO
-#define AFIO_BASE  							    0x40010000UL
-//-----------------------------
-//Base addresses for APB1 BUS Peripherals
+//User type definitions (structures)
 //-----------------------------
 
-
-//==================================================================================================
-
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral register:
-//-*-*-*-*-*-*-*-*-*-*-*
-
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral register:GPIO
-//-*-*-*-*-*-*-*-*-*-*-*
 typedef struct {
-	volatile uint32_t CRL;
-	volatile uint32_t CRH;
-	volatile uint32_t IDR;
-	volatile uint32_t ODR;
-	volatile uint32_t BSRR;
-	volatile uint32_t BRR;
-	volatile uint32_t LCKR;
-}GPIO_typeDef;
+	uint16_t GPIO_PIN_NUMBER;  //specifies the GPIO pins to be configured.
+							  //this parameter must be set based on @ref GPIO_PINS_define
 
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral register:RCC
-//-*-*-*-*-*-*-*-*-*-*-*
-typedef struct {
-	volatile uint32_t CR;
-	volatile uint32_t CFGR;
-	volatile uint32_t CIR;
-	volatile uint32_t APB2RSTR;
-	volatile uint32_t APB1RSTR;
-	volatile uint32_t AHBENR;
-	volatile uint32_t APB2ENR;
-	volatile uint32_t APB1ENR;
-	volatile uint32_t BDCR;
-	volatile uint32_t CSR;
-}RCC_typeDef;
+	uint16_t GPIO_MODE;       //specifies the operating mode for selected pins .
+	  	  	  	  	  	  	 //this parameter can value of @ref GPIO_MODE_define
 
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral register:EXTI
-//-*-*-*-*-*-*-*-*-*-*-*
-typedef struct {
-	volatile uint32_t IMR;
-	volatile uint32_t EMR;
-	volatile uint32_t RTSR;
-	volatile uint32_t FTSR;
-	volatile uint32_t SWIER;
-	volatile uint32_t PR;
-}EXTI_typeDef;
+	uint16_t GPIO_OUTPUT_SPEED ; //specifies the speed for selected pins .
+	  	  	 	 	 	 	 	//this parameter can value of @ref GPIO_SPEED_define
+}GPIO_pinConfig_t;
 
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral register:AFIO
-//-*-*-*-*-*-*-*-*-*-*-*
-typedef struct {
-	volatile uint32_t EVCR;
-	volatile uint32_t MAPR;
-	volatile uint32_t EXTICR[4];
-	uint32_t reserved0;
-	volatile uint32_t MAPR2;
-}AFIO_typeDef;
+//-----------------------------
+//Macros Configuration References
+//-----------------------------
 
-//=========================================================================================
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Peripheral Instants:
-//-*-*-*-*-*-*-*-*-*-*-*
-#define GPIOA      ((GPIO_typeDef *)GPIOA_BASE)
-#define GPIOB      ((GPIO_typeDef *)GPIOB_BASE)
-#define GPIOC      ((GPIO_typeDef *)GPIOC_BASE)
-#define GPIOD      ((GPIO_typeDef *)GPIOD_BASE)
-#define GPIOE      ((GPIO_typeDef *)GPIOE_BASE)
+/*@ref GPIO_PINS_state */
+#define GPIO_PIN_SET         1
+#define GPIO_PIN_RESET		 0
 
-#define RCC        ((RCC_typeDef  *)RCC_BASE)
-
-#define AFIO       ((AFIO_typeDef *)AFIO_BASE)
-
-#define EXTI       ((EXTI_typeDef *)EXTI_BASE)
-
-//==============================================================================================
-//-*-*-*-*-*-*-*-*-*-*-*-
-//Generic Macros:
-//-*-*-*-*-*-*-*-*-*-*-*
-
-//-*-*-*-*-*-*-*-*-*-*-*-
-//clock enable Macros:
-//-*-*-*-*-*-*-*-*-*-*-*
-#define RCC_GPIOA_CLK_EN()   (RCC->APB2ENR |= 1<<2)
-#define RCC_GPIOB_CLK_EN()   (RCC->APB2ENR |= 1<<3)
-#define RCC_GPIOC_CLK_EN()   (RCC->APB2ENR |= 1<<4)
-#define RCC_GPIOD_CLK_EN()   (RCC->APB2ENR |= 1<<5)
-#define RCC_GPIOE_CLK_EN()   (RCC->APB2ENR |= 1<<6)
-
-#define RCC_AFIO_CLK_EN()    (RCC->APB2ENR |= 1<<0 )
-
-//-*-*-*-*-*-*-*-*-*-*-*-
-//NVIC IRQ enable & disable Macros:
-//-*-*-*-*-*-*-*-*-*-*-*
-
-#define NVIC_LINE_ENABLE					1
-#define	NVIC_LINE_DISABLE					0
+/*@ref GPIO_RETURN_LOCK*/
+#define GPIO_RETURN_LOCK_OK   		1
+#define GPIO_RETURN_LOCK_ERROR 		0
 
 
-#define NVIC_IRQ6_EXTI0_ENABLE()			(NVIC_ISER0 |= 1<<6)
-#define NVIC_IRQ7_EXTI1_ENABLE()			(NVIC_ISER0 |= 1<<7)
-#define NVIC_IRQ8_EXTI2_ENABLE()			(NVIC_ISER0 |= 1<<8)
-#define NVIC_IRQ9_EXTI3_ENABLE()			(NVIC_ISER0 |= 1<<9)
-#define NVIC_IRQ10_EXTI4_ENABLE()			(NVIC_ISER0 |= 1<<10)
-#define NVIC_IRQ23_EXTI5_9_ENABLE()		    (NVIC_ISER0 |= 1<<23)
-#define NVIC_IRQ40_EXT10_15_ENABLE()		(NVIC_ISER1 |= 1<<8)
+/*@ref GPIO_PINS_define */
+#define GPIO_PIN0     	((uint16_t)  0x0001)
+#define GPIO_PIN1     	((uint16_t)  0x0002)
+#define GPIO_PIN2     	((uint16_t)  0x0004)
+#define GPIO_PIN3     	((uint16_t)  0x0008)
+#define GPIO_PIN4     	((uint16_t)  0x0010)
+#define GPIO_PIN5     	((uint16_t)  0x0020)
+#define GPIO_PIN6     	((uint16_t)  0x0040)
+#define GPIO_PIN7     	((uint16_t)  0x0080)
+#define GPIO_PIN8     	((uint16_t)  0x0100)
+#define GPIO_PIN9     	((uint16_t)  0x0200)
+#define GPIO_PIN10    	((uint16_t)  0x0400)
+#define GPIO_PIN11    	((uint16_t)  0x0800)
+#define GPIO_PIN12    	((uint16_t)  0x1000)
+#define GPIO_PIN13    	((uint16_t)  0x2000)
+#define GPIO_PIN14    	((uint16_t)  0x4000)
+#define GPIO_PIN15    	((uint16_t)  0x8000)
+#define GPIO_PIN_All  	((uint16_t)  0xFFFF)
+
+#define GPIO_PIN_MASK 	0x0000FFFU  //PIN mask for asssert test
+
+/*@ref GPIO_MODE_define*/
+
+//0: Analog mode
+//1: Floating input (reset state)
+//2: Input with pull-up
+//3: Input with pull-down
+//4: General purpose output push-pull
+//5: General purpose output Open-drain
+//6: Alternate function output Push-pull
+//7: Alternate function output Open-drain
+//8: Alternate function Input
+
+#define GPIO_MODE_ANALOG          0x00000000U
+#define GPIO_MODE_INPUT_FLO       0x00000001U
+#define GPIO_MODE_INPUT_PU        0x00000002U
+#define GPIO_MODE_INPUT_PD        0x00000003U
+#define GPIO_MODE_OUTPUT_PP       0x00000004U
+#define GPIO_MODE_OUTPUT_OD       0x00000005U
+#define GPIO_MODE_OUTPUT_AF_PP    0x00000006U
+#define GPIO_MODE_OUTPUT_AF_OD    0x00000007U
+#define GPIO_MODE_INPUT_AF_       0x00000008U
 
 
-#define NVIC_IRQ6_EXTI0_DISABLE()			(NVIC_ICER0 |= 1<<6)
-#define NVIC_IRQ7_EXTI1_DISABLE()			(NVIC_ICER0 |= 1<<7)
-#define NVIC_IRQ8_EXTI2_DISABLE()			(NVIC_ICER0 |= 1<<8)
-#define NVIC_IRQ9_EXTI3_DISABLE()			(NVIC_ICER0 |= 1<<9)
-#define NVIC_IRQ10_EXTI4_DISABLE()		    (NVIC_ICER0 |= 1<<10)
-#define NVIC_IRQ23_EXTI5_9_DISABLE()		(NVIC_ICER0 |= 1<<23)
-#define NVIC_IRQ40_EXT10_15_DISABLE()		(NVIC_ICER1 |= 1<<8)
+/*@ref GPIO_SPEED_define*/
+
+//1: Output mode, max speed 10 MHz.
+//2: Output mode, max speed 2 MHz.
+//3: Output mode, max speed 50 MHz.
+#define GPIO_SPEED_10M         	  0x00000001u
+#define GPIO_SPEED_2M         	  0x00000002u
+#define GPIO_SPEED_50M         	  0x00000003u
+
+/*
+* ===============================================
+* APIs Supported by "MCAL GPIO DRIVER"
+* ===============================================
+*/
+void MCAL_GPIO_Init(GPIO_typeDef *GPIOx , GPIO_pinConfig_t* pinConfig);
+
+void MCAL_GPIO_Deinit(GPIO_typeDef *GPIOx);
+
+uint8_t MCAL_GPIO_Readpin(GPIO_typeDef *GPIOx , uint16_t pinNumber);
+
+uint16_t MCAL_GPIO_Readport(GPIO_typeDef *GPIOx);
+
+void MCAL_GPIO_Writepin(GPIO_typeDef *GPIOx , uint16_t pinNumber, uint8_t value);
+
+void MCAL_GPIO_Writeport(GPIO_typeDef *GPIOx , uint16_t value);
+
+void MCAL_GPIO_Togglepin(GPIO_typeDef *GPIOx , uint16_t pinNumber);
+
+uint8_t MCAL_GPIO_Lockpin(GPIO_typeDef *GPIOx , uint16_t pinNumber);
 
 
-
-//-*-*-*-*-*-*-*-*-*-*-*-
-//IVT
-//-*-*-*-*-*-*-*-*-*-*-*
-//EXTI
-
-
-//Line Number
-#define EXTI0				0
-#define EXTI1				1
-#define EXTI2				2
-#define EXTI3				3
-#define EXTI4				4
-#define EXTI5				5
-#define EXTI6				6
-#define EXTI7				7
-#define EXTI8				8
-#define EXTI9				9
-#define EXTI10				10
-#define EXTI11				11
-#define EXTI12				12
-#define EXTI13				13
-#define EXTI14				14
-#define EXTI15				15
-
-
-// Interrupt request line Number
-#define 	EXTI0_IRQ			6
-#define 	EXTI1_IRQ			7
-#define 	EXTI2_IRQ			8
-#define 	EXTI3_IRQ			9
-#define 	EXTI4_IRQ			10
-#define 	EXTI5_IRQ			23
-#define 	EXTI6_IRQ			23
-#define 	EXTI7_IRQ			23
-#define 	EXTI8_IRQ			23
-#define 	EXTI9_IRQ			40
-#define 	EXTI10_IRQ			40
-#define 	EXTI11_IRQ			40
-#define 	EXTI12_IRQ			40
-#define 	EXTI13_IRQ			40
-#define 	EXTI14_IRQ			40
-#define 	EXTI15_IRQ			40
-
-
-
-
-#endif /* INC_STM32F103C6_H_ */
+#endif /* INC_STM32F103C6_DRIVER_GPIO_H_ */

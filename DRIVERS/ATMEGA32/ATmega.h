65eb0f2d965f0a441beff62c6af7eaa2768b425b
Mohamed Badr <108628976+MohamedBadr552002@users.noreply.github.com>
2023-07-05 23:06:08 +0300

    Update ATmega.h

--- DRIVERS/ATMEGA32/ATmega.h
+++ DRIVERS/ATMEGA32/ATmega.h
@@ -20,9 +20,10 @@
 //******************//
 
 #define SFIOR  *((volatile uint32_t *)0x50)			//Special FunctionIO Register
+#define SREG	*(volatile uint8_t *)(0x5F)			//Status Register
 
 
-//DIO
+////////DIO
 // PORTA
 #define PORTA	*(volatile uint8_t *)(0x3B)
 #define DDRA	*(volatile uint8_t *)(0x3A)
@@ -44,9 +45,25 @@
 #define PIND	*(volatile uint8_t *)(0x30)
 
 
+/////EXTI
+#define MCUCR	*(volatile uint8_t *)(0x55)			//MCU Control Register
+#define MCUCSR	*(volatile uint8_t *)(0x54)			//MCU Control and Status Register	
+#define GICR	*(volatile uint8_t *)(0x5B)			//General Interrupt Control Register
+#define GIFR	*(volatile uint8_t *)(0x5A)			//General Interrupt Flag Register
 
 
+////USART
+#define UDR		*(volatile uint8_t *)(0x2C)				//USART I/O Data Register
+#define UCSRA	*(volatile uint8_t *)(0x2B)				//USART Control and Status Register A
+#define UCSRB	*(volatile uint8_t *)(0x2A)				//USART Control and Status Register B
+#define UCSRC	*(volatile uint8_t *)(0x40)				//USART Control and Status Register C
+//#define UBRRH	*(volatile uint8_t *)()					//USART Baud Rate Registers UBRRH
+#define UBRRL	*(volatile uint8_t *)(0x29)				//USART Baud Rate Registers UBRRL 
 
 
+////SPI
+#define SPCR	*(volatile uint8_t *)(0x2D)			    //SPI Control Register
+#define SPSR	*(volatile uint8_t *)(0x2E)				//SPI Status Register 
+#define SPDR	*(volatile uint8_t *)(0x2F)		     	//SPI Data Register 
 
 #endif /* ATMEGA_H_ */

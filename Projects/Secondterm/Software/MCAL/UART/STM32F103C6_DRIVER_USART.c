426af8a39a6233f35063550799d636f08cde771e
Mohamed Badr <108628976+MohamedBadr552002@users.noreply.github.com>
2023-09-09 16:49:42 +0300

    Add files via upload

---
+++ Projects/Secondterm/Software/MCAL/UART/STM32F103C6_DRIVER_USART.c
@@ -0,0 +1,481 @@
+/*
+ * USART.c
+ *
+ *  Created on: APr 21, 2023
+ *      Author: Mohamed
+ */
+
+
+#include "STM32F103C6_DRIVER_USART.h"
+#include "STM32F103C6_DRIVER_RCC.h"
+#include "STM32F103C6_DRIVER_GPIO.h"
+#include "STM32F103C6_DRIVER_EXTI.h"
+
+
+
+//=================================================================
+//********************Generic Variables****************************
+//=================================================================
+
+USART_config* Global_UART1_Config ;
+USART_config* Global_UART2_Config ;
+USART_config* Global_UART3_Config ;
+
+//=================================================================
+//********************Generic Functions****************************
+//=================================================================
+
+
+void(*UARTP_IQR_Callback)(enum UART_IRQ_Event flag);
+
+
+//=================================================================
+//*********************************APIS****************************
+//=================================================================
+
+/**================================================================
+ * @Fn				-MCAL_USART_Init
+ * @brief		    -Initialize UART (Support ASYNCH only)
+ * @param [in] 		-USARTx :where x can be (1,2,3 .. depending on device used )
+ * @param [in]	    -USARTx_Config: make the configuration information for specific UART PIN
+ * @retval		    -none
+ * Note				-support now Asynch mode & Clock 8 MHz
+ *
+ */
+
+
+void MCAL_USART_INIT(USART_typeDef* USARTx , USART_config* USARTx_Config  )
+{
+
+
+	if (USARTx == USART1)
+	{ Global_UART1_Config = USARTx_Config ;}
+	else if (USARTx == USART2)
+	{ Global_UART2_Config = USARTx_Config ;}
+	else
+	{ Global_UART3_Config = USARTx_Config ;}
+
+	uint32_t PCLK , BRR ;
+
+	//CLock enable for UASRT
+	if(USARTx == USART1)
+	{ RCC_USART1_CLK_EN() ;}
+	else if(USARTx == USART2)
+	{ RCC_USART2_CLK_EN() ;}
+	else if(USARTx == USART3)
+	{ RCC_USART3_CLK_EN() ;}
+
+	//USART enable Module
+	USARTx->CR1 |=(1<<13);
+
+	//Enable TX/RX
+	USARTx->CR1 |= USARTx_Config->Mode ;
+
+	//PAYLOAD Width
+	USARTx->CR1 |= USARTx_Config->Paybad_length ;
+
+	//Parity Control bit field
+	USARTx->CR1 |= USARTx_Config->Parity_BIT ;
+
+	//number of stop Bits
+	USARTx->CR2 |= USARTx_Config->Stop_BIT ;
+
+	//Hardware flow control
+	USARTx->CR3 |= USARTx_Config->HD_Flow_Control ;
+
+
+	//configuration Baud rate
+	//USART1 		  ---->PCLK2
+	//USART2 & USART3 ---->PCLK1
+
+	if(USARTx == USART1)
+	{
+		PCLK = MCAL_RCC_GET_PCLK2_Freq();
+	}
+	else
+	{ PCLK =  MCAL_RCC_GET_PCLK1_Freq(); }
+
+	BRR = UART_BRR_Register(PCLK , USARTx_Config->Baund_rate );
+
+	USARTx->BRR = BRR ;
+
+	// Enable and Disable interrupt
+	//USART_CR1
+	if(USARTx_Config->IQR_Enable != UART_IQR_Enable_NONE)
+	{
+		USARTx->CR1 |= (USARTx_Config->IQR_Enable) ;
+		//NVIC Enable
+		if(USARTx == USART1)
+		{
+			NVIC_IRQ37_USART1_Enable ;
+		}
+		else if(USARTx == USART2)
+		{
+			NVIC_IRQ38_USART2_Enable ;
+		}
+		else if (USARTx == USART3)
+		{ NVIC_IRQ39_USART3_Enable ;}
+
+	}
+
+
+}
+
+/**================================================================
+ * @Fn				-MCAL_USART_DEINIT
+ * @brief		    -Deinit UART peripheral
+ * @param [in] 		-USARTx :where x can be (1,2,3 .. depending on device used )
+ * @retval		    -none
+ * Note				-none
+ *
+ */
+
+
+void MCAL_USART_DEINIT(USART_typeDef* USARTx )
+{
+	if (USARTx == USART1)
+	{
+		RCC_USART1_CLK_RESET();
+		NVIC_IRQ37_USART1_Disable ;
+	}
+	else if (USARTx == USART2)
+	{
+		RCC_USART2_CLK_RESET();
+		NVIC_IRQ38_USART2_Disable;
+	}
+	else if (USARTx == USART3)
+	{
+		RCC_USART3_CLK_RESET();
+		NVIC_IRQ39_USART3_Disable ;
+	}
+
+}
+
+/**================================================================
+ * @Fn				-MCAL_USART_GPIO_SET_PINS
+ * @brief		    -initialize  GPIO pins
+ * @param [in] 		-USARTx :where x can be (1,2,3 .. depending on device used )
+ * @retval		    -none
+ * Note				-should enable corresponding ALT and GPIO in RCC clock
+ *
+ */
+
+
+void MCAL_USART_GPIO_SET_PINS(USART_typeDef* USARTx)
+{
+	GPIO_pinConfig_t pincfg;
+	if (USARTx == USART1)
+	{
+		//PA9  =>TX  --> output alternative push pull
+		//PA10 =>RX	 --> alternative input
+		//PA11 =>CTS --> input floating
+		//PA12 =>CTR --> output alternative push pull
+
+
+		//PA9
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN9 ;
+		pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+		pincfg.GPIO_OUTPUT_SPEED = GPIO_SPEED_10M ;
+		MCAL_GPIO_Init(GPIOA, &pincfg);
+
+		//PA10
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN10 ;
+		pincfg.GPIO_MODE = GPIO_MODE_INPUT_AF_ ;
+		MCAL_GPIO_Init(GPIOA, &pincfg);
+
+		//PA11
+		if(Global_UART1_Config->HD_Flow_Control == USART_TX_mode || Global_UART1_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER =GPIO_PIN11 ;
+			pincfg.GPIO_MODE = GPIO_MODE_INPUT_FLO ;
+			MCAL_GPIO_Init(GPIOA, &pincfg);
+		}
+		//PA12
+		if(Global_UART1_Config->HD_Flow_Control == USART_RX_mode || Global_UART1_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER = GPIO_PIN12 ;
+			pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+			pincfg.GPIO_OUTPUT_SPEED =GPIO_SPEED_10M ;
+			MCAL_GPIO_Init(GPIOA, &pincfg);
+		}
+
+	}
+	else if (USARTx == USART2)
+	{
+		//PA2  =>TX  --> output alternative push pull
+		//PA3  =>RX	 --> alternative input
+		//PA0  =>CTS --> input floating
+		//PA1  =>CTR --> output alternative push pull
+
+
+		//PA2
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN2 ;
+		pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+		pincfg.GPIO_OUTPUT_SPEED = GPIO_SPEED_10M ;
+		MCAL_GPIO_Init(GPIOA, &pincfg);
+
+		//PA3
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN3 ;
+		pincfg.GPIO_MODE = GPIO_MODE_INPUT_AF_ ;
+		MCAL_GPIO_Init(GPIOA, &pincfg);
+
+		//PA0
+		if(Global_UART2_Config->HD_Flow_Control == USART_TX_mode || Global_UART2_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER =GPIO_PIN0 ;
+			pincfg.GPIO_MODE = GPIO_MODE_INPUT_FLO ;
+			MCAL_GPIO_Init(GPIOA, &pincfg);
+		}
+		//PA1
+		if(Global_UART2_Config->HD_Flow_Control == USART_RX_mode || Global_UART2_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER = GPIO_PIN1 ;
+			pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+			pincfg.GPIO_OUTPUT_SPEED =GPIO_SPEED_10M ;
+			MCAL_GPIO_Init(GPIOA, &pincfg);
+		}
+
+	}
+	else if (USARTx == USART3)
+	{
+		//PB10  =>TX  --> output alternative push pull
+		//PB11 =>RX	 --> alternative input
+		//PB13 =>CTS --> input floating
+		//PB14 =>CTR --> output alternative push pull
+
+
+		//PB10
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN10 ;
+		pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+		pincfg.GPIO_OUTPUT_SPEED = GPIO_SPEED_10M ;
+		MCAL_GPIO_Init(GPIOB, &pincfg);
+
+		//PB11
+		pincfg.GPIO_PIN_NUMBER = GPIO_PIN11 ;
+		pincfg.GPIO_MODE = GPIO_MODE_INPUT_AF_ ;
+		MCAL_GPIO_Init(GPIOB, &pincfg);
+
+		//PB13
+		if(Global_UART3_Config->HD_Flow_Control == USART_TX_mode || Global_UART3_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER =GPIO_PIN13 ;
+			pincfg.GPIO_MODE = GPIO_MODE_INPUT_FLO ;
+			MCAL_GPIO_Init(GPIOB, &pincfg);
+		}
+		//PB14
+		if(Global_UART3_Config->HD_Flow_Control == USART_RX_mode || Global_UART3_Config->HD_Flow_Control == USART_TX_RX_mode)
+		{
+			pincfg.GPIO_PIN_NUMBER = GPIO_PIN14 ;
+			pincfg.GPIO_MODE = GPIO_MODE_OUTPUT_AF_PP ;
+			pincfg.GPIO_OUTPUT_SPEED =GPIO_SPEED_10M ;
+			MCAL_GPIO_Init(GPIOB, &pincfg);
+		}
+
+	}
+
+
+
+}
+
+
+/**================================================================
+ * @Fn				-MCAL_USART_SEND_DATA
+ * @brief		  	-Send data by USART
+ * @param [in] 		-USARTx:	 	where x can be (1,2,3 .. depending on device used )
+ * @param [in] 		-pTXBuffer:	 	Data you want to send it
+ * @param [in] 		-polling_EN:	enable polling or diable it
+ * @retval 			-none
+ * Note				-none
+ */
+void MCAL_USART_SEND_DATA(USART_typeDef* USARTx , uint16_t* pTXBuffer , enum polling_mechanism polling_EN )
+{
+
+	if(USARTx == USART1)
+	{
+		//wait until TXE flag is set in SR
+		if(polling_EN == POLLING_Enable)
+		{
+			while(! (USARTx->SR & 1<<7) );
+		}
+
+		//Check Word length is 9BIT or 8BIT
+		if(Global_UART1_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			USARTx->DR = (*pTXBuffer & (uint16_t)0x01FF); //9 BIT
+
+		}
+		else
+		{
+			USARTx->DR = (*pTXBuffer & (uint8_t)0xFF) ;//8BIT
+		}
+
+	}
+	else if (USARTx == USART2)
+	{
+		//wait until TXE flag is set in SR
+		if(polling_EN == POLLING_Enable)
+		{
+			while(! (USARTx->SR & 1<<7) );
+		}
+
+		//Check Word length is 9BIT or 8BIT
+		if(Global_UART2_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			USARTx->DR = (*pTXBuffer & (uint16_t)0x01FF); //9 BIT
+
+		}
+		else
+		{
+			USARTx->DR = (*pTXBuffer & (uint8_t)0xFF) ;//8BIT
+		}
+
+	}
+	else if (USARTx == USART3)
+	{
+		//wait until TXE flag is set in SR
+		if(polling_EN == POLLING_Enable)
+		{
+			while(! (USARTx->SR & 1<<7) );
+		}
+
+		//Check Word length is 9BIT or 8BIT
+		if(Global_UART3_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			USARTx->DR = (*pTXBuffer & (uint16_t)0x01FF); //9 BIT
+
+		}
+		else
+		{
+			USARTx->DR = (*pTXBuffer & (uint8_t)0xFF) ;//8BIT
+		}
+
+	}
+
+}
+void MCAL_USART_WAIT_TC(USART_typeDef* USARTx)
+{
+	//Wait until TC flag is set in SR
+	//TC ------> Transmit complete
+	while(! (USARTx->SR & 1<<6));
+}
+
+
+void MCAL_USART_RECIEVE_DATA(USART_typeDef* USARTx , uint16_t* pTXBuffer , enum polling_mechanism polling_EN)
+{
+	if(USARTx == USART1)
+	{
+		//wait until RXNE is set in SR
+		if (polling_EN == POLLING_Enable)
+			while(! (USARTx->SR & 1<<5));
+
+
+		if(Global_UART1_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			if(Global_UART1_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =USARTx->DR ;
+			}
+			else
+			{
+				//9BIT== 8 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF);
+			}
+
+		}
+		else
+		{
+			if(Global_UART1_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF); //8 bit data send it all
+			}
+			else
+			{
+				//8BIT== 7 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0x7F);
+			}
+		}
+	}
+
+	else if(USARTx == USART2)
+	{
+		//wait until RXNE is set in SR
+		if (polling_EN == POLLING_Enable)
+			while(! (USARTx->SR & 1<<5));
+
+
+		if(Global_UART2_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			if(Global_UART2_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =USARTx->DR ;
+			}
+			else
+			{
+				//9BIT== 8 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF);
+			}
+
+		}
+		else
+		{
+			if(Global_UART2_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF); //8 bit data send it all
+			}
+			else
+			{
+				//8BIT== 7 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0x7F);
+			}
+		}
+	}
+	if(USARTx == USART3)
+	{
+		//wait until RXNE is set in SR
+		if (polling_EN == POLLING_Enable)
+			while(! (USARTx->SR & 1<<5));
+
+
+		if(Global_UART3_Config->Paybad_length == UART_Payload_9BIT)
+		{
+			if(Global_UART3_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =USARTx->DR ;
+			}
+			else
+			{
+				//9BIT== 8 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF);
+			}
+
+		}
+		else
+		{
+			if(Global_UART3_Config->Parity_BIT == UART_parity_Disable)
+			{
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0xFF); //8 bit data send it all
+			}
+			else
+			{
+				//8BIT== 7 Data + 1 parity(Do not consider it)
+				*((uint16_t*)pTXBuffer) =(USARTx->DR & (uint8_t)0x7F);
+			}
+		}
+	}
+
+}
+
+//ISR
+
+void USART1_IRQHandler()
+{
+	Global_UART1_Config->UARTP_IQR_Callback() ;
+}
+void USART2_IRQHandler()
+{
+	Global_UART2_Config->UARTP_IQR_Callback() ;
+}
+void USART3_IRQHandler()
+{
+	Global_UART3_Config->UARTP_IQR_Callback() ;
+}
---
+++ Projects/Secondterm/Software/MCAL/UART/STM32F103C6_DRIVER_USART.h
@@ -0,0 +1,155 @@
+/*
+ * STM32F103C6_DRIVER_USART.h
+ *
+ *  Created on: Mar 16, 2023
+ *      Author: Mohamed
+ */
+
+#ifndef INC_STM32F103C6_DRIVER_USART_H_
+#define INC_STM32F103C6_DRIVER_USART_H_
+
+#include "stm32f103c6.h"
+#include "STM32F103C6_DRIVER_RCC.h"
+#include "STM32F103C6_DRIVER_GPIO.h"
+
+
+//-----------------------------
+//User type definitions (structures)
+//-----------------------------
+typedef struct
+{
+	uint8_t   Mode ;      		//to Enable the transmission or Receiving  Mode or both
+					     	   //this parameter must be set based on @ref USART_Mode_define
+
+	uint32_t  Baund_rate;     //configures the communication Baund rate
+							 //this parameter must be set based on @ref USART_Baund_rate_define
+
+	uint8_t   Paybad_length; //specifies the number of data bit transmitted or received in frame
+							//this parameter must be set based on @ref USART_Paybad_length_define
+
+	uint8_t	  Stop_BIT; 	 //specifies the number of Stop bit in frame
+	                        //this parameter must be set based on @ref USART_Stop_BIT_define
+
+	uint8_t   Parity_BIT;	 //specifies the number of Stop bit in frame
+							//this parameter must be set based on @ref USART_Parity_BIT_define
+
+	uint8_t	  HD_Flow_Control;   //specifies if the HDflowControl is enable or not
+								//this parameter must be set based on @ref USART_HD_Flow_Control_define
+
+	uint8_t   IQR_Enable; 		//Specifies if the IQR is Enable or not
+							   //this parameter must be set based on @ref USART_IQR_Enable_define
+
+	void(*UARTP_IQR_Callback)(void); //	set the C function to be called
+
+}USART_config ;
+
+
+enum UART_IRQ_Event
+{
+	UART_IQR_TXE,
+	UART_IRQ_TC,
+	UART_IQR_TC,
+	UART_IQR_RXINE,
+	UART_IQR_ORE,
+	UART_IRQ_PE
+};
+
+
+
+//-----------------------------
+//Macros Configuration References
+//-----------------------------
+
+
+//@ref USART_Mode_define
+#define USART_TX_mode			(uint32_t)(1<<3)
+#define USART_RX_mode		    (uint32_t)(1<<2)
+#define	USART_TX_RX_mode		((uint32_t)(1<<2 | 1<<3))
+
+//@ref USART_Baund_rate_define
+#define UART_BaudRate_2400					2400
+#define	UART_BaudRate_9600					9600
+#define	UART_BaudRate_19200					19200
+#define UART_BaudRate_57600					57600
+#define UART_BaudRate_115200				115200
+#define UART_BaudRate_230400				230400
+#define UART_BaudRate_460800				460800
+#define UART_BaudRate_921600				921600
+#define UART_BaudRate_2250000				2250000
+#define UART_BaudRate_4500000				4500000
+
+//@ref USART_Paybad_length_define
+#define UART_Payload_8BIT			(uint32_t)(0<<12)
+#define UART_Payload_9BIT			(uint32_t)(1<<12)
+
+//@ref USART_Stop_BIT_define
+//00: 1 Stop bit
+//01: 0.5 Stop bit
+//10: 2 Stop bits
+//11: 1.5 Stop bit
+#define UART_StOP_1BIT			(uint32_t)(0b00<<12)
+#define UART_Stop_HALF_BIT		(uint32_t)(0b01<<12)
+#define UART_Stop_2BIT			(uint32_t)(0b10<<12)
+#define UART_Stop_1_HALF_BIT	(uint32_t)(0b11<<12)
+
+
+//@ref USART_Parity_BIT_define
+#define UART_parity_Disable		(uint32_t)(0<<10)
+#define UART_Parity_EVEN		((uint32_t)1<<10)
+#define UART_Parity_ODD		    ((uint32_t)(1<<10 | 1<<9))
+
+
+//@ref USART_HD_Flow_Control_define
+#define UART_HWFlowCtl_NONE			(uint32_t)(0)
+#define UART_HWFlowCtl_RTS			(uint32_t)(1<<8)
+#define UART_HWFlowCtl_CTS			(uint32_t)(1<<9)
+#define UART_HWFlowCtl_RTS_CTS		(uint32_t)(1<<8 | 1<<9)
+
+//@ref USART_IQR_Enable_define
+#define UART_IQR_Enable_NONE		(uint32_t)(0)
+#define UART_IQR_Enable_TXE			(uint32_t)(1<<7)//Transmit data register empty
+#define UART_IQR_Enable_TC			(uint32_t)(1<<6)//transmission complete
+#define UART_IQR_Enable_RXNETE		(uint32_t)(1<<5)//Received data ready to be read and overrun error detection
+#define UART_IQR_Enable_PE			(uint32_t)(1<<8)//Parity error
+
+
+enum polling_mechanism
+{
+	POLLING_Enable ,
+	POLLING_Disable
+};
+
+/****************MACROS FOR BAUD RATE *********/
+
+//Baud rate calculation
+// USARTDIV = fclk / (16 * Baud rate)
+//USARTDIV_MUL100 = uint32((100 *fclk ) / (16 * Baudrate) == (25 *fclk ) / (4* Baudrate) )
+//DIV_Mantissa_MUL100 = Integer Part (USARTDIV ) * 100
+//DIV_Mantissa = Integer Part (USARTDIV )
+//DIV_Fraction = (( USARTDIV_MUL100 - DIV_Mantissa_MUL100 ) * 16 ) / 100
+
+#define USARTDIV(_PCLK_ , _BAUD_)				(uint32_t)(_PCLK_/(16 * _BAUD_))
+#define USARTDIV_MUL100(_PCLK_ , _BAUD_)		(uint32_t)( (25 * _PCLK_) /(4 * _BAUD_))
+#define Mantissa_MUL100(_PCLK_ , _BAUD_)		(uint32_t)(USARTDIV(_PCLK_ , _BAUD_)* 100)
+#define Mantissa(_PCLK_ , _BAUD_)				(uint32_t)(USARTDIV(_PCLK_ , _BAUD_))
+#define DIV_Fraction(_PCLK_ , _BAUD_)			(uint32_t)(((USARTDIV_MUL100(_PCLK_ ,_BAUD_) - Mantissa_MUL100(_PCLK_ ,_BAUD_)) * 16)/100)
+#define UART_BRR_Register(_PCLK_ , _BAUD_)		((Mantissa(_PCLK_ , _BAUD_)<<4) | (DIV_Fraction(_PCLK_ , _BAUD_))& 0xF )
+
+
+
+/*
+* ===============================================
+* APIs Supported by "MCAL USART DRIVER"
+* ===============================================
+*/
+
+void MCAL_USART_INIT(USART_typeDef* USARTx , USART_config* USARTx_Config  );
+void MCAL_USART_DEINIT(USART_typeDef* USARTx );
+void MCAL_USART_GPIO_SET_PINS(USART_typeDef* USARTx);
+void MCAL_USART_SEND_DATA(USART_typeDef* USARTx , uint16_t* pTXBuffer , enum polling_mechanism polling_EN );
+void MCAL_USART_WAIT_TC(USART_typeDef* USARTx);
+void MCAL_USART_RECIEVE_DATA(USART_typeDef* USARTx , uint16_t* pTXBuffer , enum polling_mechanism polling_EN);
+
+
+
+#endif /* INC_STM32F103C6_DRIVER_USART_H_ */
